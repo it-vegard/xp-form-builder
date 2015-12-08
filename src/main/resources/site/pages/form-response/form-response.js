5eaa2090fd6cfbac6dce1d224c7a45f7a1e62c8c
Vegard Haugstvedt <it.vegard@gmail.com>
2015-12-08 04:35:39 +0100

    Add display-page for form responses. Still needs some tweaking...

---
+++ src/main/resources/site/pages/form-response/form-response.js
@@ -0,0 +1,42 @@
+var portal = require('/lib/xp/portal'); // Import the portal functions
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
+
+// Handle the GET request
+exports.get = function(req) {
+
+    // Get the content that is using the page
+    var content = portal.getContent();
+
+    var site = portal.getSite();
+
+    // Extract the main region which contains component parts
+    var mainRegion = content.page.regions.main;
+
+    var formResponse = {
+        fields: []
+    };
+    for (var field in content.data) {
+        formResponse.fields.push({
+            name: field,
+            value: content.data[field]
+        });
+    }
+    formResponse.displayName = content.displayName;
+
+    // Prepare the model that will be passed to the view
+    var model = {
+        site: site,
+        content: formResponse
+    }
+
+    // Specify the view file to use
+    var view = resolve('form-response.html');
+    
+    // Render the dynamic HTML with values from the model
+    var body = thymeleaf.render(view, model);
+
+    // Return the response object
+    return {
+        body: body
+    }
+};
